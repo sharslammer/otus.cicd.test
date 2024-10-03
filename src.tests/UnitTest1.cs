6b9d849b41742d852b910d45575efaebfbcc33b3
Anton Gerasimenko <aa.gerasimenko@gmail.com>
2024-10-03 16:05:21 +0300

    init test proj

---
+++ src.tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace src.tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test_Fail()
+        {
+            Assert.Fail();
+        }
+    }
+}
